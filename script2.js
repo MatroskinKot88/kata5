95ff8b1dcca87b9c52e4d006a539573980bde713
Matvey Filatov <8matroskinkot8@gmail.com>
2025-05-26 23:37:42 +0300

    закончил проект

--- script2.js
+++ script2.js
@@ -11,22 +11,24 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     dynamicBullet: true,
   },
-})
-}
+});
+};
 
 // КНОПКА
 const showButton = document.querySelector('.show-more-btn__button');
 const brandsGrid = document.querySelector('.brands__grid');
+const showIcon = document.querySelector('.show-more-btn__icon');
 
 showButton.addEventListener('click', function () {
-  brandsGrid.classList.toggle('..brands__grid--show');
-   
-  if (brandsGrid.classList.contains('..brands__grid--show')) {
+  brandsGrid.classList.toggle('brands__grid--show');
+  showIcon.classList.toggle('rotated');
+  
+  if (brandsGrid.classList.contains('brands__grid--show')) {
     showButton.textContent = 'Скрыть';
   } else {
     showButton.textContent = 'Показать все';
-  }
-})
-})
+  };
+});
+});
 
 
