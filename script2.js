8123c04f9e8833a7e1e1b91c319f712548b4c034
Matvey Filatov <8matroskinkot8@gmail.com>
2025-05-29 00:34:12 +0300

    внес все правки

--- script2.js
+++ script2.js
@@ -14,21 +14,20 @@ const swiper = new Swiper('.swiper', {
 });
 };
 
-// КНОПКА
-const showButton = document.querySelector('.show-more-btn__button');
+
+const showButton = document.querySelector('.show-more-btn');
 const brandsGrid = document.querySelector('.brands__grid');
+const textButton = document.querySelector('.show-more-btn__text');
 const showIcon = document.querySelector('.show-more-btn__icon');
 
 showButton.addEventListener('click', function () {
   brandsGrid.classList.toggle('brands__grid--show');
-  showIcon.classList.toggle('rotated');
-  
+  showIcon.classList.toggle('rotate');
+
   if (brandsGrid.classList.contains('brands__grid--show')) {
-    showButton.textContent = 'Скрыть';
+    textButton.textContent = 'Скрыть';
   } else {
-    showButton.textContent = 'Показать все';
+    textButton.textContent = 'Показать все';
   };
 });
-});
-
-
+})
